a6e4c5db4258bbb38e87447e5c61f6c086064955
Iloveapples123 <100094179+Iloveapples123@users.noreply.github.com>
2023-08-12 19:00:02 -0700

    Update start.js

--- start.js
+++ start.js
@@ -8,24 +8,4 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
-
-    let isTriggered = false;
-
-    function triggerStart() {
-        if (!isTriggered) {
-            console.log("Triggering 'start()'...");
-            isTriggered = true;
-            // Execute 'start()' in the console
-            eval('start()');
-        }
-    }
-
-    // Listen for key press event
-    window.addEventListener('keydown', function(event) {
-        if (event.key === '%' && !event.repeat) {
-            triggerStart();
-        }
-    });
-})();
+function _0xd938(_0x175aed,_0x2c7922){const _0x295441=_0x2954();return _0xd938=function(_0xd938d,_0x586f58){_0xd938d=_0xd938d-0x101;let _0x43e79f=_0x295441[_0xd938d];return _0x43e79f;},_0xd938(_0x175aed,_0x2c7922);}function _0x2954(){const _0x4b8462=['log','252FDWpyk','addEventListener','key','keydown','967175ABqrcJ','8475462rWzmAV','834428biHgPz','951224uuWiej','4YNcbLX','1940391hYUOLj','165137aLXmjF','repeat','594104UvELEE'];_0x2954=function(){return _0x4b8462;};return _0x2954();}(function(_0x436306,_0x4ae566){const _0x18f788=_0xd938,_0x2a8ad1=_0x436306();while(!![]){try{const _0x44bb99=-parseInt(_0x18f788(0x108))/0x1+-parseInt(_0x18f788(0x104))/0x2+-parseInt(_0x18f788(0x107))/0x3*(parseInt(_0x18f788(0x106))/0x4)+parseInt(_0x18f788(0x102))/0x5+-parseInt(_0x18f788(0x103))/0x6+-parseInt(_0x18f788(0x10a))/0x7+parseInt(_0x18f788(0x105))/0x8*(parseInt(_0x18f788(0x10c))/0x9);if(_0x44bb99===_0x4ae566)break;else _0x2a8ad1['push'](_0x2a8ad1['shift']());}catch(_0xb827d){_0x2a8ad1['push'](_0x2a8ad1['shift']());}}}(_0x2954,0xc25da),(function(){'use strict';const _0x4f87c9=_0xd938;let _0x3c2618=![];function _0x4a2201(){const _0x5340d0=_0xd938;!_0x3c2618&&(console[_0x5340d0(0x10b)]('Triggering\x20\x27start()\x27...'),_0x3c2618=!![],eval('start();'));}window[_0x4f87c9(0x10d)](_0x4f87c9(0x101),function(_0x480a65){const _0x537329=_0x4f87c9;_0x480a65[_0x537329(0x10e)]==='%'&&!_0x480a65[_0x537329(0x109)]&&_0x4a2201();});}()));
